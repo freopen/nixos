fe6eac10234e923afca02d39698848847cb7c334
Aleksei Zolotov <freopen@freopen.org>
2022-10-03 13:17:20 +0200

    Replace prometheus with OpenTememetry collector

--- server/monitoring.nix
+++ server/monitoring.nix
@@ -2,23 +2,61 @@
 {
   age.secrets.newrelic = {
     file = ../secrets/newrelic.age;
-    owner = "prometheus";
-    group = "prometheus";
+    owner = "opentelemetry";
+    group = "opentelemetry";
   };
-  services.prometheus = {
+  users.users.opentelemetry = {
+    isSystemUser = true;
+    group = "opentelemetry";
+    packages = with pkgs; [ opentelemetry-collector-contrib ];
+  };
+  users.groups.opentelemetry = { };
+  systemd.services.opentelemetry-collector =
+    let
+      otel_config = builtins.toJSON {
+        receivers = {
+          prometheus.config.scrape_configs = [{
+            job_name = "prometheus_scraper";
+            static_configs = [{
+              targets = [
+                "localhost:8888" # opentelemetry-collector
+                "localhost:9100" # prometheus-node-exporter
+              ];
+            }];
+          }];
+          journald = {};
+        };
+        service = {
+          telemetry.metrics.level = "detailed";
+          telemetry.logs.level = "debug";
+          pipelines = {
+            metrics = {
+              receivers = [ "prometheus" ];
+              processors = [];
+              exporters = [ "otlp" ];
+            };
+            logs = {
+              receivers = [ "journald" ];
+              processors = [];
+              exporters = [ "otlp" ];
+            };
+          };
+        };
+      };
+    in
+    {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      restartTriggers = [ pkgs.opentelemetry-collector-contrib otel_config ];
+      serviceConfig = {
+        User = "opentelemetry";
+        ExecStart = 
+            "${pkgs.opentelemetry-collector-contrib}/bin/otelcontribcol" + 
+            " --config=" + builtins.toFile "config.yaml" otel_config +
+            " --config=" + config.age.secrets.newrelic.path;
+      };
+    };
+  services.prometheus.exporters.node = {
     enable = true;
-    scrapeConfigs = [
-      {
-        job_name = "node";
-        static_configs = [{
-          targets = ["localhost:9100"];
-        }];
-      }
-    ];
-    exporters.node.enable = true;
-    remoteWrite = [{
-      url = "https://metric-api.eu.newrelic.com/prometheus/v1/write?prometheus_server=server";
-      bearer_token_file = config.age.secrets.newrelic.path;
-    }];
   };
 }

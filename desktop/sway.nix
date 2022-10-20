b5fe16711817ac84953a14b2ea582f957930aad6
Aleksei Zolotov <freopen@freopen.org>
2022-10-20 10:00:09 +0200

    Add Mako notification system

--- desktop/sway.nix
+++ desktop/sway.nix
@@ -2,20 +2,25 @@
 {
   programs.sway.enable = true;
 
-  home-manager.users.freopen.wayland.windowManager.sway = {
-    enable = true;
-    config = {
-      modifier = "Mod4";
-      defaultWorkspace = "workspace number 1";
-      terminal = "alacritty";
-      window = {
-        titlebar = false;
-        hideEdgeBorders = "both";
+  home-manager.users.freopen = {
+    programs.mako = {
+      enable = true;
+    };
+    wayland.windowManager.sway = {
+      enable = true;
+      config = {
+        modifier = "Mod4";
+        defaultWorkspace = "workspace number 1";
+        terminal = "alacritty";
+        window = {
+          titlebar = false;
+          hideEdgeBorders = "both";
+        };
+        keybindings = lib.mkOptionDefault {
+          "XF86MonBrightnessUp" = "brightnessctl set +5%";
+          "XF86MonBrightnessDown" = "brightnessctl set -5%";
+        };
       };
-      keybindings = lib.mkOptionDefault {
-        "XF86MonBrightnessUp" = "brightnessctl set +5%";
-        "XF86MonBrightnessDown" = "brightnessctl set -5%";
-      }; 
     };
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";

af8313bee71d55efd05f1843dc5a6401e8f62cd4
Aleksei Zolotov <freopen@freopen.org>
2023-01-04 12:52:36 +0100

    Set i3status rust modules

--- laptop/sway.nix
+++ laptop/sway.nix
@@ -21,7 +21,52 @@ in {
     };
     programs.i3status-rust = {
       enable = true;
-      bars.default = { icons = "material-nf"; };
+      bars.default = {
+        icons = "material-nf";
+        blocks = [
+          {
+            block = "focused_window";
+            show_marks = "visible";
+            max_width = 200;
+          }
+          {
+            block = "keyboard_layout";
+            driver = "sway";
+            format = "{layout^2}";
+          }
+          {
+            block = "load";
+            format = "{1m} {5m} {15m}";
+          }
+          {
+            block = "temperature";
+            collapsed = false;
+            format = "{max}";
+          }
+          {
+            block = "cpu";
+            format = "{barchart} {frequency}";
+          }
+          { block = "memory"; }
+          { block = "disk_space"; }
+          {
+            block = "net";
+            format =
+              "{ssid} {signal_strength} {speed_down;K*B} {graph_down} {speed_up;K*B} {graph_up}";
+          }
+          { block = "sound"; }
+          { block = "backlight"; }
+          {
+            block = "battery";
+            format = "{percentage} {time} {power}";
+          }
+          {
+            block = "time";
+            format = "%a %F %T";
+            interval = 1;
+          }
+        ];
+      };
     };
     wayland.windowManager.sway = {
       enable = true;

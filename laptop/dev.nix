0ae09790ffd414b8885daf609fe4f4f6f17fad68
Aleksei Zolotov <freopen@freopen.org>
2023-05-01 09:09:16 +0200

    Fix cargo completions

--- laptop/dev.nix
+++ laptop/dev.nix
@@ -1,4 +1,19 @@
-{ pkgs, fenix, agenix, ... }: {
+{ pkgs, fenix, agenix, ... }:
+let
+  rust = (with fenix.packages.x86_64-linux;
+    (combine [
+      complete.cargo
+      complete.clippy
+      complete.rust-src
+      complete.rustc
+      complete.rustfmt
+      targets.wasm32-unknown-unknown.latest.rust-std
+    ]));
+  rust-completions = pkgs.symlinkJoin {
+    name = "rust-completions";
+    paths = [ rust ];
+  };
+in {
   programs.nix-ld.enable = true;
   home-manager.users.freopen = {
     nixpkgs.overlays = [ fenix.overlays.default ];
@@ -8,15 +23,8 @@
         clang
         nil
         nixfmt
-        (with fenix.packages.x86_64-linux;
-          (combine [
-            complete.cargo
-            complete.clippy
-            complete.rust-src
-            complete.rustc
-            complete.rustfmt
-            targets.wasm32-unknown-unknown.latest.rust-std
-          ]))
+        rust
+        rust-completions
       ];
       sessionVariables = { LIBCLANG_PATH = "${pkgs.libclang.lib}/lib"; };
     };

f57e74929a7bbf2aa25939e006ed728c444f7872
Aleksei Zolotov <freopen@freopen.org>
2023-08-26 08:32:59 +0200

    Install Minecraft

--- laptop/gaming.nix
+++ laptop/gaming.nix
@@ -1,25 +1,29 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.steam = {
     enable = true;
     gamescopeSession = { enable = true; };
   };
   programs.gamescope = { capSysNice = true; };
-  home-manager.users.freopen.programs.mangohud = {
-    enable = true;
-    settings = {
-      toggle_hud = "F12";
-      font_size = 12;
-      hud_compact = true;
-      hud_no_margin = true;
-      battery = true;
-      gamepad_battery = true;
-      cpu_power = true;
-      cpu_temp = true;
-      gpu_power = true;
-      gpu_temp = true;
-      ram = true;
-      vram = true;
-      resolution = true;
+  home-manager.users.freopen = {
+    home.packages = with pkgs; [ prismlauncher minecraft ];
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        toggle_hud = "F12";
+        font_size = 12;
+        hud_compact = true;
+        hud_no_margin = true;
+        battery = true;
+        gamepad_battery = true;
+        cpu_power = true;
+        cpu_temp = true;
+        gpu_power = true;
+        gpu_temp = true;
+        ram = true;
+        vram = true;
+        resolution = true;
+      };
     };
   };
+
 }

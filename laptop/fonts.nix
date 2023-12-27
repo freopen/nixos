bdc650c7426397767ad565dcdfbcdfdc1d3d442d
Aleksei Zolotov <freopen@freopen.org>
2023-12-27 10:28:52 +0100

    Switch font to Iosevka

--- laptop/fonts.nix
+++ laptop/fonts.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }: {
   fonts = {
     packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+      (nerdfonts.override { fonts = [ "IosevkaTerm" ]; })
+      (iosevka-bin.override { variant = "aile"; })
+      (iosevka-bin.override { variant = "etoile"; })
       noto-fonts
       noto-fonts-emoji
     ];
     fontconfig = {
       defaultFonts = {
-        monospace = [ "Sauce Code Pro Nerd Font" ];
-        serif = [ "Noto Serif" ];
-        sansSerif = [ "Noto Sans" ];
+        monospace = [ "IosevkaTerm Nerd Font" ];
+        serif = [ "Iosevka Etoile" "Noto Serif" ];
+        sansSerif = [ "Iosevka Aile" "Noto Sans" ];
         emoji = [ "Noto Color Emoji" ];
       };
       subpixel.rgba = "rgb";
@@ -17,4 +19,3 @@
     };
   };
 }
-

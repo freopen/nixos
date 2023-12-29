7d06cbdaba7724543c991d9c2ed856d4e1ffe177
Aleksei Zolotov <freopen@freopen.org>
2023-12-29 10:58:00 +0100

    Switch dev rust to rustup provided

--- laptop/dev.nix
+++ laptop/dev.nix
@@ -1,41 +1,20 @@
-{ pkgs, fenix, agenix, ... }:
-let
-  rust = (with fenix.packages.x86_64-linux;
-    (combine [
-      complete.cargo
-      complete.clippy
-      complete.rust-src
-      complete.rustc
-      complete.rustfmt
-      complete.llvm-tools-preview
-      targets.wasm32-unknown-unknown.latest.rust-std
-      targets.thumbv6m-none-eabi.latest.rust-std
-    ]));
-  rust-completions = pkgs.symlinkJoin {
-    name = "rust-completions";
-    paths = [ rust ];
-  };
-in {
+{ pkgs, agenix, ... }: {
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   programs.nix-ld.enable = true;
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", MODE="0664", GROUP="plugdev"
   '';
   home-manager.users.freopen = {
-    nixpkgs.overlays = [ fenix.overlays.default ];
     home = {
       packages = with pkgs; [
         agenix.packages.x86_64-linux.default
         cargo-binutils
-        cargo-bolero
         cargo-insta
         cargo-make
         clang
         nil
         nixfmt
-        probe-rs
-        rust
-        rust-completions
+        rustup
         qmk
       ];
       sessionVariables = { LIBCLANG_PATH = "${pkgs.libclang.lib}/lib"; };

4b014a1f802f0bd9cc9d0b6f5ef0f7220adab148
Aleksei Zolotov <freopen@freopen.org>
2024-02-02 09:30:08 +0100

    Set recommended Nginx settings on fp0

--- raspi/nginx.nix
+++ raspi/nginx.nix
@@ -2,12 +2,10 @@
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   services.nginx = {
     enable = true;
-    # recommendedTlsSettings = true;
-    # recommendedOptimisation = true;
-    # recommendedProxySettings = true;
-    # recommendedGzipSettings = true;
-    # recommendedBrotliSettings = true;
-    # recommendedZstdSettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedGzipSettings = true;
   };
   security.acme = {
     acceptTerms = true;

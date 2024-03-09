eef0ee5b8aadfd3ec86013bab73bfcffb232a7ab
Aleksei Zolotov <freopen@freopen.org>
2024-03-09 10:58:00 +0100

    Reduce update freq for smartd

--- raspi/netdata.nix
+++ raspi/netdata.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, lib, ... }: {
   age.secrets.netdata_stream_fp0 = {
     file = ../secrets/netdata_stream_fp0.age;
     owner = "netdata";
@@ -16,7 +16,13 @@
       };
       ml.enabled = false;
     };
-    configDir = { "stream.conf" = config.age.secrets.netdata_stream_fp0.path; };
+    configDir = (builtins.mapAttrs (file: config:
+      builtins.toFile (builtins.baseNameOf file)
+      (lib.generators.toYAML { } config)) {
+        "python.d/smartd_log.conf" = { update_every = 60 * 60; };
+      }) // {
+        "stream.conf" = config.age.secrets.netdata_stream_fp0.path;
+      };
   };
   environment.persistence."/persist".files =
     [ "/var/lib/netdata/registry/netdata.public.unique.id" ];

3fa04aed8f4371156075d9580d4e4dedfb0b0012
Aleksei Zolotov <freopen@freopen.org>
2024-05-04 11:08:03 +0200

    Add cargo based afl++, tmux

--- laptop/dev.nix
+++ laptop/dev.nix
@@ -1,4 +1,6 @@
-{ pkgs, agenix, ... }: {
+{ pkgs, agenix, ... }:
+let afl-fuzz = pkgs.writeShellScriptBin "afl-fuzz" "cargo-afl afl fuzz $@";
+in {
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   virtualisation.vmVariant.virtualisation.cores = 15;
   programs.nix-ld.enable = true;
@@ -6,30 +8,31 @@
     SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", MODE="0664", GROUP="plugdev"
   '';
   environment = {
-    variables = { CARGO_INSTALL_ROOT = "~/.local"; };
+    variables = { CARGO_INSTALL_ROOT = "/home/freopen/.local"; };
     localBinInPath = true;
   };
   home-manager.users.freopen = {
     home = {
-      packages = [ agenix.packages.x86_64-linux.default ] ++ (with pkgs; [
-        unstable.aflplusplus
-        binaryen
-        cargo-binutils
-        cargo-insta
-        cargo-make
-        cargo-update
-        clang
-        nil
-        nixcfg-apply
-        unstable.nixd
-        nixfmt
-        nodePackages.pnpm
-        protobuf
-        rustup
-        qmk
-        wasm-bindgen-cli
-        wasm-pack
-      ]);
+      packages = [ agenix.packages.x86_64-linux.default afl-fuzz ]
+        ++ (with pkgs; [
+          binaryen
+          cargo-binutils
+          cargo-insta
+          cargo-make
+          cargo-update
+          clang
+          nil
+          nixcfg-apply
+          unstable.nixd
+          nixfmt
+          nodePackages.pnpm
+          protobuf
+          rustup
+          qmk
+          tmux
+          wasm-bindgen-cli
+          wasm-pack
+        ]);
       sessionVariables = { LIBCLANG_PATH = "${pkgs.libclang.lib}/lib"; };
     };
     programs = {

54a765c1338710b0c11838bae303b81cb9d8befa
Aleksei Zolotov <freopen@freopen.org>
2024-10-10 09:50:14 +0200

    Miniflux to repeat broken feeds and to record metrics

--- fv2/miniflux.nix
+++ fv2/miniflux.nix
@@ -9,10 +9,15 @@
   services.miniflux = {
     enable = true;
     config = {
-      LISTEN_ADDR = "127.0.0.1:6000";
       BASE_URL = "https://rss.freopen.org";
-      CLEANUP_ARCHIVE_UNREAD_DAYS = "-1";
-      CLEANUP_ARCHIVE_READ_DAYS = "730";
+      CLEANUP_ARCHIVE_READ_DAYS = 2 * 365;
+      CLEANUP_ARCHIVE_UNREAD_DAYS = -1;
+      CLEANUP_FREQUENCY_HOURS = 30 * 24;
+      LISTEN_ADDR = "127.0.0.1:6000";
+      METRICS_COLLECTOR = 1;
+      POLLING_PARSING_ERROR_LIMIT = 0;
+      POLLING_SCHEDULER = "entry_frequency";
+      SCHEDULER_ENTRY_FREQUENCY_MIN_INTERVAL = 60;
     };
     adminCredentialsFile = config.age.secrets.miniflux.path;
   };
@@ -21,4 +26,5 @@
     enableACME = true;
     locations."/".proxyPass = "http://127.0.0.1:6000/";
   };
+  services.netdata.metrics.miniflux = 6000;
 }

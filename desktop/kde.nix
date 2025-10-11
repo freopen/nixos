8fb8fa0754a0770e581a872675fdee1df4c145d7
Aleksei Zolotov <freopen@freopen.org>
2025-10-11 17:13:32 +0200

    Store SSH password in kwallet

--- desktop/kde.nix
+++ desktop/kde.nix
@@ -1,4 +1,8 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+  programs.ssh.enableAskPassword = true;
+  programs.ssh.askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
+  environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
 }

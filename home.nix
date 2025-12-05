27c9ab1da26af8a6bc31f78ab1310f5e5e54adcd
Aleksei Zolotov <freopen@freopen.org>
2025-12-05 14:49:10 +0100

    More standalone HM settings

--- home.nix
+++ home.nix
@@ -1,14 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home = {
     username = "freopen";
     homeDirectory = "/home/freopen";
     stateVersion = "25.11";
     packages = with pkgs; [
+      nerd-fonts.iosevka-term
       nixd
       nixfmt-rfc-style
       rustup
+      sysz
     ];
+    shell.enableZshIntegration = true;
   };
 
   programs = {
@@ -21,7 +24,67 @@
         user.email = "freopen@freopen.org";
       };
     };
+    zsh = {
+      enable = true;
+      autocd = true;
+      autosuggestion = {
+        enable = true;
+        strategy = [
+          "match_prev_cmd"
+          "completion"
+        ];
+      };
+      enableVteIntegration = true;
+      history = {
+        append = true;
+        expireDuplicatesFirst = true;
+        extended = true;
+      };
+      historySubstringSearch.enable = true;
+      initContent = lib.mkOrder 550 ''
+        # Add system completions to fpath
+        fpath+=("/usr/share/zsh/site-functions")
+        fpath+=("/usr/share/zsh/vendor-completions")
+        fpath+=("/usr/share/zsh/$ZSH_VERSION/functions")
+      '';
+      syntaxHighlighting = {
+        enable = true;
+      };
+    };
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    fzf.enable = true;
+  };
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "IosevkaTerm Nerd Font" ];
+      serif = [
+        "Noto Serif"
+      ];
+      sansSerif = [
+        "Noto Sans"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+    subpixelRendering = "rgb";
+    hinting = "full";
   };
   nixpkgs.config.allowUnfree = true;
+  services.home-manager = {
+    autoExpire = {
+      enable = true;
+      frequency = "weekly";
+      store.cleanup = true;
+    };
+    autoUpgrade = {
+      enable = true;
+      frequency = "weekly";
+      useFlake = true;
+      flakeDir = "/home/freopen/Documents/nixos";
+    };
+  };
   news.display = "silent";
 }
